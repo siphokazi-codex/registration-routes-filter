c4cd5dbcd4f0bcb7fee9fb1248fd13673be55ea0
siphokazi-codex <aplenis@webmail.co.za>
2017-09-28 17:41:55 +0200

    Modified my tests files

--- test/model.test.js
+++ test/model.test.js
@@ -1,42 +1,66 @@
 const assert = require('assert');
 const Models = require('../models');
 
-describe('models should be able to', function(){
+describe('models should be able to', function() {
 
-  var models = Models('mongodb://localhost/registration');
+  var models = Models('mongodb://localhost/registration-test');
 
-    //  beforeEach(function(done) {
-    //   models.Registration.remove({}, function(err){
-    //      done(err);
-    //    })
-    //  });
+  beforeEach(function(done) {
+    models.Registration.remove({}, function(err, results) {
+      done(err);
+    })
+  });
 
-  it('store registration entered by user to MongoDB', function(done){
+  it('store registration entered by user to MongoDB', function(done) {
 
-    var regData = { registration : 'The test registration'};
+    var regData = {
+      registration: 'The test registration'
+    };
     models.Registration
-      .create(regData, function(err){
-        if (err){
+      .create(regData, function(err) {
+        if (err) {
           return done(err);
         }
         //done(err);
 
-         models.Registration.find({registration : 'The registration test'}, function(err, registration){
-            assert.equal(1, registration.length);
-            done(err);
-         });
+        models.Registration.find(regData, function(err, registration) {
+          assert.equal(1, registration.length);
+          done(err);
+        });
       });
   });
 
-     it('should not allow duplicate registration numbers', function(done){
-       var regData = { registration : 'The registration test'};
-       models.Registration.create(regData, function(err){
+  it('should not allow duplicate registration numbers', function(done) {
 
-           assert.ok(err, 'Should give an error for duplicate registration numbers');
-           done();
 
-       });
-     });
+    var regData = {
+      registration: 'The test registration'
+    };
+    models.Registration
+      .create(regData, function(err) {
+        if (err) {
+          return done(err);
+        }
+        //done(err);
+        models.Registration.create(regData, function(err, registration) {
+          assert.ok(err, "Duplicate value exception should have been thrown!");
+          //  assert.equal(1, registration.length);
+          done();
+        });
+
+      });
+
+
+    //  var regData = { registration : 'The registration test'};
+    //
+    //  models.Registration.create(regData, function(err){
+    //
+    //      assert.ok(err, 'Should give an error for duplicate registration numbers');
+    //      done();
+    //
+    //  });
+
+  });
 
 
 });
